434878dbab2efa9198bcb88e0a61860c8e1a2390
kimsung jun <koke007@hanmail.net>
2016-08-16 19:37:45 +0900

    First Commit
    첫 번째 커밋~ 테스트

---
+++ First_Project/First_Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  First_Project
+//
+//  Created by kimsung jun on 2016. 8. 16..
+//  Copyright © 2016년 kimsung jun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
